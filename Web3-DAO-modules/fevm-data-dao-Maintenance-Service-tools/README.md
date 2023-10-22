53dd6645909c3e0c45af71e7672282b6732f7031
seetadev <dev@seeta.in>
2023-10-23 00:41:34 +0530

    datadao fevm module

---
+++ Web3-DAO-modules/fevm-data-dao-Maintenance-Service-tools/README.md
@@ -0,0 +1,22 @@
+# FEVM-Data-DAO for Service and Repair Work at Incident sites
+
+We are extending the beta kit to  build a basic Decentralized Autonomous Organization (DAO) on Filecoin for Service and Repair Work at Incident sites.
+
+DAOs create a RFP for providing service or maintenance on the dapp. 
+
+Service providers can join a RFP by minting an NFT of that RFP. 
+
+Service providers that have a certain RFP's NFT are eligible to create proposals and vote on them. 
+
+Voting is gasless and the vote is stored on IPFS and Filecoin with the most recent vote linking to one before.
+
+Please visit https://drive.google.com/drive/u/1/folders/1Bt24sL_XSbyFjg3XT6_rx-_EZDhYDw1s
+
+
+## About This Repo
+
+Start out in the "hardhat" directory and follow the readme there to begin deploying and interacting with contracts.
+
+Then go to the "frontend" directory to run an example frontend!
+
+
---
+++ Web3-DAO-modules/fevm-data-dao-Maintenance-Service-tools/hardhat/hardhat.config.js
@@ -0,0 +1,39 @@
+require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-deploy")
+require("hardhat-deploy-ethers")
+require("./tasks")
+require("dotenv").config()
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+    solidity: {
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+            details: { yul: false },
+          },
+        },
+      },
+    defaultNetwork: "Calibration",
+    networks: {
+        Calibration: {
+            chainId: 314159,
+            url: "https://api.calibration.node.glif.io/rpc/v1",
+            accounts: [PRIVATE_KEY],
+        },
+        FilecoinMainnet: {
+            chainId: 314,
+            url: "https://api.node.glif.io",
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts",
+    },
+}
